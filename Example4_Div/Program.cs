cfa8d0acd3ba016badc05288cd171958576a2093
Anton Tishchenko <AntonTishchenk0@yandex.ru>
2023-07-03 01:06:41 +0200

    add Div

---
+++ Example4_Div/Program.cs
@@ -0,0 +1,4 @@
+﻿double numberA = 12;      //int если целое число
+double numberB = 5;       //int если целое число
+double result = numberA / numberB;   //int если целое число
+Console.WriteLine(result);
